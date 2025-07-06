a19d17877de08e0fa1a87a531eb96aad410d852d
RalucaZota <zotaraluca649@gmail.com>
2025-07-06 15:40:21 +0300

    did logic and finished challenge

--- clock/index.js
+++ clock/index.js
@@ -1,37 +1,22 @@
 const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
-let counter = 90;
 
-const initialCall = () => {
-  //   setHour();
-  //   setMinutes();
-  setSeconds();
-};
-const setHour = () => {
-  setInterval(() => {
-    const d = new Date();
-    let hour = d.getHours();
-    console.log(d, 'd');
-    hourHand.style.transform = 'rotate(360deg)';
-    hourHand.style.transition = 'transform 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)';
-    console.log('aici');
-    console.log(hour);
-  }, 1000);
-};
-
-const setMinutes = () => {
+const updateClock = () => {
   const d = new Date();
-  let minute = d.getMinutes();
+
+  const hour = d.getHours();
+  const minute = d.getMinutes();
+  const second = d.getSeconds();
+  
+  const hourWinkle = (hour % 12) * 30 + minute * 0.5;
+  const minuteWinkle = minute * 6;
+  const secondWinkle = second * 6;
+
+  hourHand.style.transform = `rotate(${hourWinkle}deg)`;
+  minuteHand.style.transform = `rotate(${minuteWinkle}deg)`;
+  secondHand.style.transform = `rotate(${secondWinkle}deg)`;
 };
 
-const setSeconds = () => {
-  setInterval(() => {
-    const d = new Date();
-    let second = d.getSeconds();
-    console.log(second, 'second');
-    secondHand.style.transform = 'rotate(360deg)';
-    secondHand.style.transition = 'transform 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)';
-    console.log('aici');
-  }, 1000);
-};
-// initialCall();
+setInterval(updateClock, 1000);
+updateClock();
